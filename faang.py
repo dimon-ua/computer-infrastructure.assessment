e4939dd3e119a5548db55624be55631b1bdcc7cf
Dima Kozlovskyy <dima.kozlovskyy@gmail.com>
2025-12-15 20:39:26 +0000

    Python script called faang.py in the root of your repository

---
+++ faang.py
@@ -0,0 +1,45 @@
+# FAANG stock data plotting script.
+import yfinance as yf
+
+# Date and time handling.
+import datetime as dt
+
+# Data frames.
+import pandas as pd
+
+# Plotting.
+import matplotlib.pyplot as plt
+
+# Listing files in a folder.
+import os
+
+def plot_data():
+    """
+    Reads the latest CSV file from the data folder, plots the Close prices 
+    for all five stocks on a single figure, adds labels and a title, and 
+    saves the plot as a timestamped PNG file in the plots folder.
+    """
+
+    # Get current date and time.
+    now = dt.datetime.now()
+
+    # List all files in the data folder.
+    data_files = os.listdir('data/')
+
+    # Sort files
+    sorted_data_files = sorted(data_files, reverse=True)
+
+    # Read the most recent file.
+    df = pd.read_csv('data/' + sorted_data_files[0], header=[0,1], index_col=0, parse_dates=True)
+
+    # Plot new figure and axis.
+    fig, ax = plt.subplots()
+
+    # Plot five stocks
+    df['Close'].plot(ax=ax)
+
+    # Titles and axes
+    ax.set_xlabel("Date")
+    ax.set_ylabel("Closing Price (USD)")
+    ax.legend(title="Ticker")
+    ax.set_title(df.index[-1].strftime("%Y-%m-%d"))
